896a0d550b31aa4f5a7eecc5ac58e0a0313eafdc
Gabriel a fono <55008545+Gabriel-Andy@users.noreply.github.com>
2021-08-21 12:22:13 -0400

    Add files via upload
    I created new constructors for dnata

---
+++ series/object.js
@@ -0,0 +1,9 @@
+let Dnata = function(name,id,status,hiredDate){
+    this.name = name;
+    this.id = id;
+    this.status = status;
+    this.hiredDate = hiredDate
+}
+
+let Andy = new Dnata('gabriel', 2197600, "supervisor", 'march 28, 2013')
+console.log(Andy)
